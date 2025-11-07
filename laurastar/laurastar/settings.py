9c811ce2dae262bbdf50546085bf45e514381d93
Seyitnur <seyitnur.a.atayev.tm@gmail.com>
2025-11-07 21:45:26 +0500

    new

--- laurastar/laurastar/settings.py
+++ laurastar/laurastar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yja%&_&kpg8=#id%@v0=6jrqruivb-mw8t6^kjc#o_dv$v3ii-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
